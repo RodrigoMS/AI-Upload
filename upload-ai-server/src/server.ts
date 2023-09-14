4f4eccb6f216e42dfdc0705a2537897b7f1cc4a4
RodrigoMS <informaticarodrigomessias@gmail.com>
2023-09-13 20:34:29 -0400

    Generator AI completion and  and transcription

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -2,19 +2,19 @@ import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
-//import { createTranscriptionRoute } from "./routes/create-transcription";
-//import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
 
 app.register(fastifyCors, {
-origin: '*'
+    origin: '*'
 })
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
-//app.register(createTranscriptionRoute)
-//app.register(generateAiCompletionRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
     port: 3000,
